ca190891a6cf2bc1f00b3e50985ebfbb49992b85
snehalnp <snehalpalve1998@gmail.com>
2024-05-05 11:31:38 +0530

    changes in setup.py file

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='weather_sdk',
-    version='0.1',
+    name="weather-sdk",
+    version="0.1",
     packages=find_packages(),
-    package_dir={'': '.'},
-    install_requires=[
-        'requests',
-    ],
+    install_requires=["requests"],
+    author="Snehal Palve",
+    description="A simple SDK for interacting with the OpenWeatherMap API",
+    url="https://github.com/snehalnp/weather-sdk",
 )
